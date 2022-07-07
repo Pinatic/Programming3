5627c59cf4425b4bbad4456ebda79ec5daeaac38
Pinatic <Pieterdejong812@gmail.com>
2022-07-07 10:05:35 +0200

    Update Assignment3.sh

--- Assignment3/Assignment3.sh
+++ Assignment3/Assignment3.sh
@@ -6,3 +6,4 @@ mkdir -p output
 
 for n in {1..16} ; do /usr/bin/time -o timings.txt --append -f "${n}\t%e" blastp -query MCRA.faa -db refseq_protein/refseq_protein -num_threads $n -outfmt 6 >> blastoutput.txt ; done
 
+python3 assignment3.py
