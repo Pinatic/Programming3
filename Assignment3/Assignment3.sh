5ea1cd801d1c604cbefcc44f7d3071bc840cea1f
Pinatic <Pieterdejong812@gmail.com>
2022-07-07 10:04:08 +0200

    Create Assignment3.sh

---
+++ Assignment3/Assignment3.sh
@@ -0,0 +1,8 @@
+#bash script that saves timings from using 1 till 16 threads
+
+export BLASTDB=/local-fs/datasets/
+
+mkdir -p output
+
+for n in {1..16} ; do /usr/bin/time -o timings.txt --append -f "${n}\t%e" blastp -query MCRA.faa -db refseq_protein/refseq_protein -num_threads $n -outfmt 6 >> blastoutput.txt ; done
+
