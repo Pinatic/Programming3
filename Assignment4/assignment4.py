ef1979c66fb2579b788ac0f19a08c0e16416345a
Pinatic <43998564+Pinatic@users.noreply.github.com>
2022-10-10 16:40:30 +0200

    Update assignment4.py
    run .sh to get the input files.

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -2,10 +2,9 @@ from Bio import SeqIO
 import os
 import sys
 
-input = sys.stdin
-output = sys.stdout #as a simple number
-N50 = 
-
 def calc_N50():
   
 if __name__ = "__main__":
+  input = sys.stdin
+  output = sys.stdout #as a simple number
+  N50 = 
