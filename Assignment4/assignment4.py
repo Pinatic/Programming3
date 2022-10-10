05fcbf85e58256b845131b7be21fd8de9e0fde5b
Pinatic <Pieterdejong812@gmail.com>
2022-10-10 17:00:18 +0200

    added stat of parser to get just the sequence Lines

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 def contig_parser(input):
-  seq_list = []
+  seq_list_unfiltered = [str(line.strip()) for line in input]
+  seq_list_filtered = [line for line in seq_list_unfiltered if not line.startswith(">")]
 
 def calc_N50():
   
