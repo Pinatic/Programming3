cd6cb05931ca445046667423b606f258db9914fc
Pinatic <Pieterdejong812@gmail.com>
2022-10-11 16:04:04 +0200

    removed filtering out > because they are needed for the lenghts of the sequences. made function to append all sequence lengths to a list. added calculation of N50

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -3,13 +3,37 @@ import os
 import sys
 
 def contig_parser(input):
-  seq_list_unfiltered = [str(line.strip()) for line in input]
-  seq_list_filtered = [line for line in seq_list_unfiltered if not line.startswith(">")]
-  seq_list_filtered.sort(key=len)
+    #appends the length of each sequence to a list
+    seq_lengths = []
+    seq_list_unfiltered = SeqIO.to_dict(SeqIO.parse(input, "fasta"))
 
-def calc_N50():
-  
-if __name__ = "__main__":
+    for seq in seq_list_unfiltered.items():
+        seq_lengths.append(len(seq[1].seq))
+    
+    seq_lengths.sort(key=len)
+
+    return seq_lengths
+
+def calculate_N50(seq_lengths):
+    """Calculate N50 for a sequence of numbers.
+    Args:list_of_lengths (list): List of numbers.
+    Returns:float: N50 value.
+ 
+    """
+    tmp = []
+    for tmp_number in set(seq_lengths):
+            tmp += [tmp_number] * seq_lengths.count(tmp_number) * tmp_number
+    tmp.sort()
+ 
+    if (len(tmp) % 2) == 0:
+        median = (tmp[int(len(tmp) / 2) - 1] + tmp[int(len(tmp) / 2)]) / 2
+    else:
+        median = tmp[int(len(tmp) / 2)]
+ 
+    return median
+
+if __name__ == "__main__":
   input = sys.stdin
   output = sys.stdout #as a simple number
-  N50 = 
+  N50 = str(calculate_N50(contig_parser(input)))
+  output.write(f"{N50}, ")
