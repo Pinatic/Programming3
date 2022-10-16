1c5309d6c3edd34e1d37782e241991f783076c40
Pinatic <Pieterdejong812@gmail.com>
2022-10-16 15:32:23 +0200

    using SeqIO to make a dict from the fasta and get the lengths from the dict seems not to work well. getting lengths line by line now

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -1,28 +1,33 @@
-from Bio import SeqIO
 import os
 import sys
 
 def contig_parser(input_file):
     #appends the length of each sequence to a list
+
+    seq_list_unfiltered = [str(line.strip()) for line in input_file]
+    tmp_seq_list = []
     seq_lengths = []
-    seq_dict = {rec.id : rec.seq for rec in SeqIO.parse(input_file, "fasta")}
 
-    #seq_list_unfiltered = SeqIO.to_dict(SeqIO.parse(input, "fasta"))
+    for line in seq_list_unfiltered:
+        if not line.startswith(">"):
+            tmp_seq_list.append(line)
+        else:
+            seq_lengths.append(tmp_seq_list)
+            tmp_seq_list = []
 
-    for seq in seq_dict.items():
-        seq_lengths.append(len(seq[1].seq))
+    seq_lengths_list = [len(seq) for seq in seq_lengths]
 
-    return seq_lengths
+    return seq_lengths_list
 
-def calculate_N50(seq_lengths):
+def calculate_N50(seq_lengths_list):
     """Calculate N50 for a sequence of numbers.
     Args:list_of_lengths (list): List of numbers.
     Returns:float: N50 value.
  
     """
     tmp = []
-    for tmp_number in set(seq_lengths):
-            tmp += [tmp_number] * seq_lengths.count(tmp_number) * tmp_number
+    for tmp_number in set(seq_lengths_list):
+            tmp += [tmp_number] * seq_lengths_list.count(tmp_number) * tmp_number
     tmp.sort()
  
     if (len(tmp) % 2) == 0:
