eb4319b0ac8841665383b43315b23ce3687d9ba0
Pinatic <Pieterdejong812@gmail.com>
2022-10-16 16:36:57 +0200

    made small change to get len using key in dict

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -7,8 +7,8 @@ def contig_parser(input_file):
     seq_lengths_list = []
     
     record_dict = SeqIO.to_dict(SeqIO.parse(input_file, "fasta"))
-    for key in record_dict.items():
-        seq_lengths_list.append(len(key[1].seq))
+    for key in record_dict.keys():
+        seq_lengths_list.append(len(record_dict[key]))
 
     return seq_lengths_list
 
