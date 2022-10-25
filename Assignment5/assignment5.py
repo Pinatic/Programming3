75d997914054d51f191f23e65e35e463075386f8
Pinatic <Pieterdejong812@gmail.com>
2022-10-25 11:18:01 +0200

    Q3 did not work out very well. now uses explode from pyspark

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -42,8 +42,8 @@ class InterPRO_PS:
         Q2 = [2, Q2_answer, Q2_explain]
 
         #3. What is the most common GO Term found?
-        Q3_explain = df.groupBy("_c13").agg(df._c13.filter(df._c13 != "-")).orderBy(desc("count"))._jdf.queryExecution().toString()
-        Q3_answer = df.groupBy("_c13").agg(df._c13.filter(df._c13 != "-")).orderBy(desc("count")).take(1)
+        Q3_explain = df.withColumn("_c13", explode(split(col("_c13"), "\\|"))).select("_c13").filter(df._c13 != "-").agg({"_c13": "max"})._jdf.queryExecution().toString()
+        Q3_answer = df.withColumn("_c13", explode(split(col("_c13"), "\\|"))).select("_c13").filter(df._c13 != "-").agg({"_c13": "max"}).collect()[0][0]
         Q3 = [3, Q3_answer, Q3_explain]
 
         #4. What is the average size of an InterPRO feature found in the dataset?
