2eb2f134c11eda63b39b4d933653a180d3863954
Pinatic <Pieterdejong812@gmail.com>
2023-01-16 14:50:57 +0100

    Remove no large and small
    Added function that takes proteins that have at least 1 large and 1 small feature
    Started on report

--- Assignment6/assignment6.py
+++ Assignment6/assignment6.py
@@ -9,7 +9,7 @@ start = time.time()
 
 #path = "/data/dataprocessing/interproscan/all_bacilli.tsv"
 
-def file_loader(self, path, sep):
+def file_loader(path, sep):
     """
     Loads in the data using the path and sep
     only loads in columns 0, 2, 6, 7, 11
@@ -22,7 +22,7 @@ def file_loader(self, path, sep):
     
     return ddf
 
-def cleaner(self, ddf):
+def cleaner(ddf):
     """
     removes rows containing - in collumn Interpro_acc
     drops duplicates
@@ -46,13 +46,23 @@ def coverage_calc(ddf):
     Size = (ddf["Stop"] - ddf["Start"]) / ddf["Seq_lenght"]
     return Size
 
-def 
+def remove_no_large(ddf):
+    """
+    Returns proteins that have a large feature (>90% of the proteins sequence)
+    And that also have at least one small feature
+    use with .apply() method
 
+    returns dask dataframe 
+    """
+    if (ddf["Size"] > 0.90).any() and (ddf["Size"] < 0.90).any():
+        return ddf
 
 
 if __name__ == "__main__":
     ddf = file_loader(path, "\t")
     ddf = cleaner(ddf)
     ddf["Size"] = ddf.apply(lambda x:coverage_calc(x), axis = 1)
+    #Taking only the proteins that have a large interpro accession
+    ddf = ddf.groupby("Protein_acc").apply(remove_no_large).reset_index(drop = True)
 
 
